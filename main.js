e2bc520015dcb1331c3b66297795b47ee67c2665
Matissoss <vateuszptaszynski@proton.me>
2024-12-09 16:43:40 +0100

    better css

--- main.js
+++ main.js
@@ -33,12 +33,23 @@ function contains(str, chr){
 	return false;
 }
 
+function is_mobile() {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    const isTouchScreen = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+    return (
+        /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent) || 
+        isTouchScreen
+    );
+}
+
+
 let file_conts = await get_file("/data/pl.csv");
 let vec = await csvfn(file_conts);
 let index = Math.floor(Math.random() * 5500);
 // Const's declaration
 let won = false;
-const ANSWER = vec[index];
+const ANSWER = vec[index].toUpperCase();
 const KEYBOARD_ELEMENT = $("keyboard");
 const ANSWER_ELEMENT = $("answer");
 const ROWS = [$("r1"), $("r2"), $("r3"), $("r4"), $("r5"), $("r6")];
@@ -53,8 +64,9 @@ for (let i = 0; i < KEYBOARD_ELEMENT.childElementCount; i++){
 	const ROW = KEYBOARD_ELEMENT.children[i];
 	for (let j = 0; j < ROW.childElementCount; j++){
 		const BUTTON = ROW.children[j];
+		BUTTON.id = BUTTON.innerHTML;
 		BUTTON.addEventListener("click", () => {
-			handle_keypress(BUTTON.innerHTML);
+			handle_keypress(BUTTON.innerHTML.toUpperCase());
 		});
 	}
 }
@@ -74,24 +86,37 @@ function set_currentlypressed(){
 		}
 	}
 }
-if(navigator.platform.toLowerCase() != "android"|"ios"){
+if(!is_mobile()){
 document.addEventListener("keydown", function (event) {
     const key = event.key;
     if (!won&&VALID_KEYS.includes(key) || SPECIAL_KEYS[key]) {
-        handle_keypress(key);
+        handle_keypress(key.toUpperCase());
     }
 });
 }
 
-export function handle_keypress(key) {
-    if (key==="Bac"||key === "Backspace" && !won) {
+function handle_keypress(key) {
+    if (key==="BAC"||key === "BACKSPACE" && !won) {
         remove_last_character();
-    } else if (key === "Ent"||key === "Enter" && !won) {
+    } 
+    else if (key === "ENT"||key === "ENTER" && !won) {
         if (check_all()) {
             handle_enter();
         }
-    } else if (VALID_KEYS.includes(key) && !won) {
+    } 
+    else if (VALID_KEYS.includes(key) && !won) {
         insert_character(key);
+	$(key).animate(
+		[
+        		{ background: "#eeeeee"},
+        		{ background: "lightgray" }
+    		],
+    		{
+        		duration: 70,
+        		iterations: 1,
+        		easing: "ease-in-out"
+    		}
+	);
     }
 }
 
@@ -150,10 +175,13 @@ function check_all(){
 	let can_proceed = true;
 	for (let i = 0; i < currently_sellected.childElementCount; i++){
 		if (currently_sellected.children[i].innerHTML ==""){
-			$("msg").innerHTML = "Wejście nie jest pełne";
 			can_proceed = false;
 		}
 	}
+	if (can_proceed){
+		$("msg").innerHTML = "";
+	}
+	else{$("msg").innerHTML = "Wejście nie jest pełne!"}
 	return can_proceed;
 }
-ANSWER_ELEMENT.innerHTML = vec[index];
+ANSWER_ELEMENT.innerHTML = vec[index].toUpperCase();
